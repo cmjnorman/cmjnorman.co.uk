98f700500e169a7b61b0a68269bf4976fb4b5c53
Christopher Norman <cmjnorman@gmail.com>
2021-01-05 18:00:15 +0000

    Add script.js with functions to run navigation buttons

---
+++ script.js
@@ -0,0 +1,28 @@
+function goToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function goToAbout() {
+    document.getElementById("about").scrollIntoView({
+        behavior: "auto",
+        block: "center",
+        inline: "center"
+    });
+}
+
+function goToProjects() {
+    document.getElementById("projects").scrollIntoView({
+        behavior: "auto",
+        block: "center",
+        inline: "center"
+    });
+}
+
+function goToContact() {
+    document.getElementById("contact").scrollIntoView({
+        behavior: "auto",
+        block: "center",
+        inline: "center"
+    });
+}
